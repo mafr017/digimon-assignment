91241dc9ace25d41e2fab6cf0ca66df57843f773
Muhammad Aditya Fathur Rahman <fathur.rahman@ikonsultan.com>
2026-04-10 16:31:13 +0700

    Initial Commit

---
+++ digimon-assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  digimon-assignment
+//
+//  Created by Muhammad Aditya Fathur Rahman on 10/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
